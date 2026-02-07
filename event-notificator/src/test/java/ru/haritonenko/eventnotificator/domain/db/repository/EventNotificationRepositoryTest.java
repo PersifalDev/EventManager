8bc64d891730e15a02d73f7e11548b66e4e8711e
harit <haritonenko04@mail.ru>
2026-02-07 12:09:16 +0700

    Remove .env from repository

---
+++ event-notificator/src/test/java/ru/haritonenko/eventnotificator/domain/db/repository/EventNotificationRepositoryTest.java
@@ -0,0 +1,30 @@
+package ru.haritonenko.eventnotificator.domain.db.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventNotificationRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findAllUnredNotificationsByUserId() {
+    }
+
+    @Test
+    void markUserNotificationsAsRead() {
+    }
+
+    @Test
+    void deleteByCreatedAtBefore() {
+    }
+}
